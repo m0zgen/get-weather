1ee7d8c84cde06e4c8220c0501cc123a14303202
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2016-08-01 11:07:22 +0600

    Update script parse data

--- get-weather.sh
+++ get-weather.sh
@@ -3,15 +3,15 @@
 # Get weather from yandex
 
 # Get yandex weather xml
-curl -s https://export.yandex.ru/bar/reginfo.xml?region=Almaty > ya.xml && sed -i -n '/<day_part typeid=\"1\"/,/<\/day_part>/p' ya.xml
+curl -s https://export.yandex.ru/bar/reginfo.xml?region=Almaty > ya.xml && sed -i -n '/<day_part typeid=/,/<\/day_part>/p' ya.xml
 
-# Get weather types
+# # Get weather types
 TYPE=$(cat ya.xml | grep weather_type | sed -e 's/<[^>]*>//g')
 TEMP=$(cat ya.xml | grep temperature | sed -e 's/<[^>]*>//g' | head -1)
 CITY=$(cat ya.xml | grep time_zone | sed -e 's/<[^>]*>//g')
 
-# Remove spaces
+# # Remove spaces
 RESULT=$(echo "$CITY $TYPE $TEMP°" | tr -s " ")
 
-# Data to screen
+# # Data to screen
 echo $RESULT
